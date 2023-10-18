6f8e68811deb33f0cdffa78f38542d2df704222b
redihoxhaa <redi.hoxha1996@gmail.com>
2023-10-18 02:05:58 +0200

    feat: bonus parziale

---
+++ function.js
@@ -0,0 +1,10 @@
+function showHiddenMenu() {
+    var menuShow = document.getElementById("hidden-menu");
+    if (menuShow.style.display === "block") {
+        menuShow.style.display = "none";
+    } else {
+        menuShow.style.display = "block";
+    }
+}
+
+
